ff0efba9f3cfd2b2ab00bf09932052415a5a7741
Gyorgy Szombathelyi <gyorgy.szombathelyi@doclerholding.com>
2015-10-22 11:51:10 +0200

    Support empty host groups

--- openstack-installer/inventory/inventory.py
+++ openstack-installer/inventory/inventory.py
@@ -8,7 +8,7 @@ def get_parent_group(yml, group, nesting_level):
     nesting_level+=1
     if nesting_level == 10:
         raise(Exception, "Too many nesting level of groups, probably a loop?")
-    if 'inherit' in yml[group]:
+    if yml[group] and 'inherit' in yml[group]:
         return get_parent_group(yml, yml[group]['inherit'], nesting_level)
     else:
         return group
@@ -20,7 +20,7 @@ def inventory(hostname):
 
     if hostname:
         for group in yml:
-            if hostname in yml[group]:
+            if yml[group] and hostname in yml[group]:
                 hostvars=yml[group][hostname]
                 hostvars['ansible_ssh_host']=hostvars['ip']['mgmt']
                 print json.dumps(hostvars, indent=4)
@@ -29,7 +29,7 @@ def inventory(hostname):
         inventory={}
         for group in yml:
             groupname=get_parent_group(yml, group,0)
-            inventory[group]=sorted(yml[groupname].keys())
+            inventory[group]=sorted(yml[groupname].keys()) if yml[groupname] else []
 
         print json.dumps(inventory, indent=4)
 
