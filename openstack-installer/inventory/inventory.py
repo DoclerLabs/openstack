2c901bc9a5274101a3dc58f26054962485c06b40
Gyorgy Szombathelyi <gyorgy.szombathelyi@doclerholding.com>
2015-12-14 10:35:24 +0100

    Make pep8 happy

--- openstack-installer/inventory/inventory.py
+++ openstack-installer/inventory/inventory.py
@@ -8,49 +8,56 @@ import json
 
 MAX_NESTING_LEVEL = 10
 
+
 def expand_group(yml, group, nesting_level):
 
-    nesting_level+=1
+    nesting_level += 1
     if nesting_level == MAX_NESTING_LEVEL:
         raise(Exception, "Too many nesting level of groups, probably a loop?")
-    hosts=[]
+    hosts = []
     if yml[group]:
         for host, key in yml[group].iteritems():
             if host == 'inherit':
-                if isinstance(key,list):
+                if isinstance(key, list):
                     for inherited_group in key:
-                        hosts.extend(expand_group(yml,inherited_group,nesting_level))
+                        hosts.extend(
+                            expand_group(yml, inherited_group, nesting_level))
                 else:
-                     hosts.extend(expand_group(yml,key,nesting_level))
+                    hosts.extend(expand_group(yml, key, nesting_level))
             else:
                 hosts.append(host)
     return hosts
 
+
 def inventory(hostname):
 
     with open(os.path.dirname(sys.argv[0])+"/inventory.yml", 'r') as f:
-        yml=yaml.safe_load(f)
+        yml = yaml.safe_load(f)
 
     if hostname:
         for group, data in yml.iteritems():
             if data and hostname in data:
-                hostvars=data[hostname]
-                hostvars['ansible_ssh_host']=hostvars['ip']['mgmt']
+                hostvars = data[hostname]
+                hostvars['ansible_ssh_host'] = hostvars['ip']['mgmt']
                 print json.dumps(hostvars, indent=4)
                 return
     else:
-        inventory={ "_meta": { "hostvars" : {} } }
+        inventory = {"_meta": {"hostvars": {}}}
         for group, data in yml.iteritems():
-            inventory[group]=sorted(expand_group(yml,group,0))
-            if data:
-                for host, hostvars in data.iteritems():
-                    if host != 'inherit':
-                        inventory['_meta']['hostvars'][host]=hostvars
-                        inventory['_meta']['hostvars'][host]['ansible_ssh_host']=hostvars['ip']['mgmt']
+            inventory[group] = sorted(expand_group(yml, group, 0))
+            if data is None:
+                continue
+            for host, hostvars in data.iteritems():
+                if host == 'inherit':
+                    continue
+                inventory['_meta']['hostvars'][host] = hostvars
+                inventory['_meta']['hostvars'][host]['ansible_ssh_host'] = \
+                    hostvars['ip']['mgmt']
 
         print json.dumps(inventory, indent=4)
 
-parser = argparse.ArgumentParser(description='Dynamic inventory for Openstack.')
+parser = argparse.ArgumentParser(
+    description='Dynamic inventory for Openstack.')
 parser.add_argument('--list', help='list the hosts', action='store_true')
 parser.add_argument('--host', help='returnt the variables for the hosts')
 args = parser.parse_args()
--- openstack-installer/scripts/generate_secrets.py
+++ openstack-installer/scripts/generate_secrets.py
@@ -8,14 +8,19 @@ import string
 
 PASSWORD_LEN = 32
 
+
 def generate_secrets(filename):
 
-    secrets=[]
+    secrets = []
     with open(filename, 'r') as f:
         for line in f:
-            match = re.search('^([a-zA-Z].*):\s*$',line)
+            match = re.search('^([a-zA-Z].*):\s*$', line)
             if match:
-                password=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(PASSWORD_LEN))
+                password = ''.join(random.SystemRandom().
+                                   choice(string.ascii_uppercase +
+                                          string.ascii_lowercase +
+                                          string.digits)
+                                   for _ in range(PASSWORD_LEN))
                 secrets.append(match.group(1) + ': ' + password + '\n')
             else:
                 secrets.append(line)
