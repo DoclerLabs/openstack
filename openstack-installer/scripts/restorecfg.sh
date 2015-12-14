6be60e50ef6c9cf71a9de815642e40a1be35a652
Gyorgy Szombathelyi <gyorgy.szombathelyi@doclerholding.com>
2015-12-14 12:19:32 +0100

    Add config save and restore scripts

---
+++ openstack-installer/scripts/restorecfg.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 config_name"
+    echo
+    echo "$0: restores the saved config 'config_name' from configs"
+    exit 0
+fi
+
+BASEDIR=$(dirname "$0")/..
+CONFDIR="$BASEDIR/configs/$1"
+cp "$CONFDIR"/group_vars/all/* group_vars/all
+cp "$CONFDIR/inventory/inventory.yml" "inventory/inventory.yml"
---
+++ openstack-installer/scripts/savecfg.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 config_name"
+    echo
+    echo "$0: saves the current configs as config_name to configs"
+    exit 0
+fi
+
+BASEDIR=$(dirname "$0")/..
+CONFDIR="$BASEDIR/configs/$1"
+mkdir -p "$CONFDIR"/{group_vars/all,inventory}
+cp group_vars/all/* "$CONFDIR/group_vars/all"
+cp "inventory/inventory.yml" "$CONFDIR/inventory/inventory.yml"
