e41f6f5ec91bcd3a679e84e28a3e326d4a875e3f
Gyorgy Szombathelyi <gyorgy.szombathelyi@doclerholding.com>
2016-03-25 11:44:14 +0100

    Scripts: save/restore files/ directory
    The files directory can be used to store artifacts, like
    certificates to install. Since it is a standard ansible
    directory, these artifacts can be referred without any path given.

--- openstack-installer/scripts/restorecfg.sh
+++ openstack-installer/scripts/restorecfg.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 if [ -z "$1" ]; then
     echo "Usage: $0 config_name"
@@ -11,5 +11,7 @@ BASEDIR=$(dirname "$0")/..
 CONFDIR="$BASEDIR/configs/$1"
 cp "$CONFDIR"/group_vars/all/* "$BASEDIR"/group_vars/all
 cp "$CONFDIR/inventory/inventory.yml" "$BASEDIR/inventory/inventory.yml"
+rm -rf "$BASEDIR"/files/* 2>/dev/null || /bin/true
+cp -r "$CONFDIR"/files "$BASEDIR" || /bin/true
 grep -q "^\$ANSIBLE_VAULT" "$BASEDIR"/group_vars/all/secrets.yml && ansible-vault decrypt "$BASEDIR"/group_vars/all/secrets.yml
 rm "$BASEDIR/workdir/"* 2>/dev/null || /bin/true
--- openstack-installer/scripts/savecfg.sh
+++ openstack-installer/scripts/savecfg.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 if [ -z "$1" ]; then
     echo "Usage: $0 config_name [-e|--encrypt]"
@@ -10,9 +10,10 @@ fi
 
 BASEDIR=$(dirname "$0")/..
 CONFDIR="$BASEDIR/configs/$1"
-mkdir -p "$CONFDIR"/group_vars/all "$CONFDIR"/inventory
+mkdir -p "$CONFDIR"/group_vars/all "$CONFDIR"/inventory "$CONFDIR"/files
 cp "$BASEDIR"/group_vars/all/* "$CONFDIR/group_vars/all"
 cp "$BASEDIR/inventory/inventory.yml" "$CONFDIR/inventory/inventory.yml"
+cp -r "$BASEDIR/files" "$CONFDIR"
 if [ "$2" = "-e" -o "$2" = "--encrypt" ]; then
     ansible-vault encrypt "$CONFDIR/group_vars/all/secrets.yml"
 fi
