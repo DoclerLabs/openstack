e75c8372d58fcdc1d90524ae13e65a5be20383b7
Gyorgy Szombathelyi <gyorgy.szombathelyi@doclerholding.com>
2016-07-26 12:34:14 +0200

    Nagios: add rabbitmq monitoring

---
+++ openstack-monitoring/roles/nrpe/files/check_rabbitmq_cluster.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+import subprocess
+import re
+import yaml
+
+NAGIOS_OK = 0
+NAGIOS_WARN = 1
+NAGIOS_CRIT = 2
+NAGIOS_UNKNOWN = 3
+NAGIOS_STATUS = {
+    NAGIOS_OK: "OK",
+    NAGIOS_WARN: "Warning",
+    NAGIOS_CRIT: "Critical",
+    NAGIOS_UNKNOWN: "Unknown"}
+
+
+def get_rabbitmq_nodes():
+
+    proc = subprocess.Popen(["/usr/sbin/rabbitmqctl", "cluster_status"],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            shell=False)
+    (out, err) = proc.communicate()
+    if proc.returncode != 0:
+        raise Exception(err)
+
+    # remove first line
+    status = out[out.find('\n')+1:]
+    # erl to yaml
+    yml_s = re.sub('{([a-z].*?),', r'{"\1":', status)
+    yml = yaml.safe_load(yml_s)
+
+    disc_nodes = ram_nodes = running_nodes = partitions = []
+    for section in yml:
+        if 'nodes' in section:
+            for kind in section['nodes']:
+                if 'disc' in kind:
+                    disc_nodes = kind['disc']
+                elif 'ram' in kind:
+                    ram_nodes = kind['ram']
+        elif 'running_nodes' in section:
+            running_nodes = section['running_nodes']
+        elif 'partitions' in section:
+            partitions = section['partitions']
+    return disc_nodes, ram_nodes, running_nodes, partitions
+
+
+def main():
+
+    try:
+        (disc_nodes, ram_nodes, running_nodes, partitions) = \
+            get_rabbitmq_nodes()
+        if not running_nodes:
+            ret = NAGIOS_CRIT
+            msg = "No running nodes!"
+        elif partitions:
+            ret = NAGIOS_WARN
+            msg = "Partitions: %s" % partitions
+        elif sorted(disc_nodes + ram_nodes) != sorted(running_nodes):
+            ret = NAGIOS_CRIT
+            msg = "Disc nodes: %s, RAM nodes: %s, running nodes: %s" % \
+                  (disc_nodes, ram_nodes, running_nodes)
+        else:
+            ret = NAGIOS_OK
+            msg = "Disc nodes: %s, RAM nodes: %s" % (disc_nodes, ram_nodes)
+    except Exception, e:
+        ret = NAGIOS_UNKNOWN
+        msg = str(e)
+
+    print "%d %s %s" % (ret, NAGIOS_STATUS[ret],  msg)
+    exit(ret)
+
+main()
